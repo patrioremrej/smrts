555994cbd6c6ae142606ecc3a48e1e51a22ddcf9
Sam <freestff0077@gmail.com>
2025-08-26 02:14:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ API_HASH = getenv("API_HASH", "2e19c326d8cb322df7c15d7b7e84d1f3")
 BOT_TOKEN = getenv("BOT_TOKEN", "7853877617:AAEGA7xLZIv0FJiXbvd2pCyXNlNs9PL1vLI") #8419506999:AAGHxYoprCCZIc1Cam2bk0ecHRiwLl9gkJw
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7794867502 7852142757").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://fixmayart834:FMWwXBd4JJYMs2Iv@cluster0.ltpube9.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP = getenv("LOG_GROUP", "-1002867430238")
+LOG_GROUP = getenv("LOG_GROUP", "-1002879768901")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002879768901"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
