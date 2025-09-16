1ce60928d33127a6715f1cc47bfa1358937aee3f
patrioremrej <patrioremrej@gmail.com>
2025-09-16 21:30:43 +0530

    Update config_db.py

--- snigdha/core/mongo/config_db.py
+++ snigdha/core/mongo/config_db.py
@@ -3,13 +3,14 @@ from config import MONGO_DB
 
 mongo = AsyncIOMotorClient(MONGO_DB)
 db = mongo.bot_config
+log_config = db.log_config  # New collection for log group info
 
 async def set_log_group(log_group_id):
-    await db.update_one({'_id': 'LOG_GROUP'}, {'$set': {'value': log_group_id}}, upsert=True)
+    await log_config.update_one({'_id': 'LOG_GROUP'}, {'$set': {'value': log_group_id}}, upsert=True)
 
 async def get_log_group():
-    doc = await db.find_one({'_id': 'LOG_GROUP'})
+    doc = await log_config.find_one({'_id': 'LOG_GROUP'})
     return doc['value'] if doc else None
 
 async def delete_log_group():
-    await db.delete_one({'_id': 'LOG_GROUP'})
+    await log_config.delete_one({'_id': 'LOG_GROUP'})
